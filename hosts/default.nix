7bbb0f975a88017862d62ffd355dd9294ba3e222
- <->
2025-05-28 06:47:57 +1000

    add stylix and shell

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,3 @@
-# hosts/default.nix
 { inputs, nixpkgs, system }:
 
 let
@@ -27,60 +26,5 @@ in
     };
   };
 
-
-
-  # nixos = nixpkgs.lib.nixosSystem {
-  #     inherit system;
-  #     modules = [
-  #       ../os
-  #     ];
-  #   };
-
-  # homeConfigurations = {
-  #   mark = {
-  #     pkgs = inputs.nixpkgs.legacyPackages.${system};
-  #     modules = [
-  #       ../home
-  #     ];
-  #     extraSpecialArgs = {
-  #     inherit inputs;
-  #     };
-  #   };
-  # };
-
-  # flake.nixosConfigurations = {
-  #   nixos = inputs.nixpkgs.lib.nixosSystem {
-  #     inherit system;
-  #     modules = [
-  #       (import ../os)
-  #       #../os/system/configuration.nix
-  #       # Optionally add home-manager here if using it as a NixOS module:
-  #               # Enable Home Manager as a NixOS module
-  #       inputs.home-manager.nixosModules.home-manager
-  #       {
-  #         home-manager.useGlobalPkgs = true;
-  #         home-manager.useUserPackages = true;
-  #         home-manager.users.mark = import ./../home { 
-  #           inherit inputs system;
-  #           pkgs = inputs.nixpkgs.legacyPackages.${system};};
-  #       }
-  #     ];
-  #   };
-  # };
-
-  # homeConfigurations = {
-  #   mark = inputs.home-manager.lib.homeManagerConfiguration {
-  #     # inherit system;
-  #     pkgs = inputs.nixpkgs.legacyPackages.${system};
-  #     modules = [
-  #       ../home
-  #       inputs.nixvim.homeManagerModules.nixvim
-  #     ];
-  #     extraSpecialArgs = {
-  #       user = "mark";
-  #       inherit inputs;  # Add this line to pass inputs
-  #     };
-  #   };
-  # };
 }
 
