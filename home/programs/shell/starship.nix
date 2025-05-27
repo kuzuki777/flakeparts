880e70ec57becf76cad3ab7f2bc97befe40aaa31
- <->
2025-05-28 07:36:41 +1000

    add styleix

--- home/programs/shell/starship.nix
+++ home/programs/shell/starship.nix
@@ -15,26 +15,6 @@
       $character \
       """
 
-      palette = "rose-pine"
-
-      [palettes.rose-pine]
-      overlay = '${base01}'
-      love = '${base08}'
-      gold = '${base0A}'
-      rose = '${base07}'
-      pine = '${base0D}'
-      foam = '${base0B}'
-      iris = '${base0E}'
-
-      [character]
-      format = "$symbol "
-      success_symbol = "[╰─](bold iris)[ ](bold iris)"
-      error_symbol = "[╰─](bold iris)[ ](bold love)"
-      vimcmd_symbol = "[╰─](bold iris)[ ](bold foam)"
-      vimcmd_visual_symbol = "[╰─](bold iris)[ ](bold pine)"
-      vimcmd_replace_symbol = "[╰─](bold iris)[ ](bold gold)"
-      vimcmd_replace_one_symbol = "[╰─](bold iris)[ ](bold gold)"
-
 
 
       
