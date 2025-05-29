ab04a868bd873447eebc69617dd164122bea6e6e
Mark.Wang <mark.wang@gallagher.com>
2025-05-30 06:33:30 +1000

    add wallpaper

--- home/default.nix
+++ home/default.nix
@@ -48,15 +48,15 @@
 
   stylix = {
     enable = true;
-    autoEnable = false;
+    # autoEnable = false;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     # overlays = {
     #   enable = true;
     # };
     image = pkgs.fetchurl {
-      url = "https://i.imgur.com/c3CWmia.jpg";
-      sha256 = "0lgqwjl6jd1y84cz368s4sq0krzg67znqxirzapqxqvfdpn9rwbw";
-    };
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+   };
     # polarity = "dark";
     # targets = {
     #   console.enable = true;
