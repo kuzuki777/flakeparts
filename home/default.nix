090eeab6f8862b1b6b72a4be9bfd530ddf07b26b
Mark <markwangfreetyle@gmail.com>
2025-05-30 07:03:24 +1000

    added wallpaper

--- home/default.nix
+++ home/default.nix
@@ -50,14 +50,11 @@
     enable = true;
     # autoEnable = false;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    # overlays = {
-    #   enable = true;
-    # };
     image = pkgs.fetchurl {
-      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-      hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-   };
-    # polarity = "dark";
+     url = "https://i.imgur.com/c3CWmia.jpeg";
+     sha256 = "0lgqwjl6jd1y84cz368s4sq0krzg67znqxirzapqxqvfdpn9rwbw";
+    };
+    polarity = "dark";
     # targets = {
     #   console.enable = true;
     #   gnome.enable = true;
